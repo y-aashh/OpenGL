4ecdfee6fd5af2ab1eebd2844177f73bb0d95294
y-aashh <40933032+y-aashh@users.noreply.github.com>
2019-04-23 10:39:42 +0530

    Add files via upload

--- cube.c
+++ cube.c
@@ -1,51 +1,88 @@
 #include<stdlib.h>
 #include<GLUT/glut.h>
-GLfloat vertices[][3] = {{-1.0,-1.0,-1.0},{1.0,-1.0,-1.0},{1.0,1.0,-1.0},{-1.0,1.0,-1.0},{-1.0,-1.0,1.0},{1.0,-1.0,1.0},{1.0,1.0,1.0},{-1.0,1.0,1.0}};
-GLfloat colors[][3] = {{0.0,0.0,0.0},{1.0,0.0,0.0},{1.0,1.0,0.0},{1.0,1.0,0.0},
-{0.0,0.0,1.0},{1.0,0.0,1.0},{1.0,1.0,1.0},{0.0,1.0,1.0}};
-
-static GLfloat theta[] = {0.0,0.0,0.0};
-static GLint axis =2;
 
 
-void polygon(int a, int b, int c, int d)
+static GLfloat theta = 0.0f;
+float yt = -3;
+
+void polygon()
 {
             glBegin(GL_POLYGON);
-            glColor3fv(colors[a]);
-            glVertex3fv(vertices[a]);
-            glColor3fv(colors[b]);
-            glVertex3fv(vertices[b]);
-            glColor3fv(colors[c]);
-            glVertex3fv(vertices[c]);
-            glColor3fv(colors[d]);
-            glVertex3fv(vertices[d]);
+            glColor3f(1.0f,0.0f,0.0f);//r
+            glVertex3i(0,0,0);
+            glVertex3i(1,0,0);
+            glVertex3i(1,1,0);
+            glVertex3i(0,1,0);
             glEnd();
+
+            glBegin(GL_POLYGON);
+            glColor3f(0.0f,1.0f,0.0f);//g
+            glVertex3i(0,0,1);
+            glVertex3i(1,0,1);
+            glVertex3i(1,1,1);
+            glVertex3i(0,1,1);
+            glEnd();
+
+            glBegin(GL_POLYGON);
+            glColor3f(0.0f,0.0f,1.0f);//b
+            glVertex3i(0,0,1);
+            glVertex3i(0,1,1);
+            glVertex3i(0,1,0);
+            glVertex3i(0,0,0);
+            glEnd();
+
+            glBegin(GL_POLYGON);
+            glColor3f(1.0f,1.0f,0.0f);//rg
+            glVertex3i(0,1,1);
+            glVertex3i(0,1,0);
+            glVertex3i(1,1,0);
+            glVertex3i(1,1,1);
+            glEnd();
+
+            glBegin(GL_POLYGON);
+            glColor3f(1.0f,0.0f,1.0f);//rb
+            glVertex3i(1,1,1);
+            glVertex3i(1,1,0);
+            glVertex3i(1,0,0);
+            glVertex3i(1,0,1);
+            glEnd();
+
+            glBegin(GL_POLYGON);
+            glColor3f(0.0f,1.0f,1.0f);//gb
+            glVertex3i(1,0,1);
+            glVertex3i(1,0,0);
+            glVertex3i(0,0,0);
+            glVertex3i(0,0,1);
+            glEnd();
+
+            glLineWidth(10);
+            glBegin(GL_LINES);
+            glColor3f(1,1,1);
+            glVertex3f(-5,0.5,0.5);
+            glVertex3f(6,0.5,0.5);
+            glEnd();
+
+            
+
 }
 
-
-
-
-
-void colorcube()
-{           
-            polygon(0,3,2,1);
-            polygon(2,3,7,6);
-            polygon(0,4,7,3);
-            polygon(1,2,6,5);
-            polygon(4,5,6,7);
-            polygon(0,1,5,4);
-}
-
-
-
-void spincube()
+void translate()
 {
-theta[axis]+=2.0;
-if(theta[axis]>360.0)
-theta[axis]-=360.0;
-glutPostRedisplay();
+  theta+=4.0;
+    if(theta>360.0)
+        theta-=360.0;
 
+   if(yt<3)
+        yt+=0.025;
+    else
+    {
+            yt = -3;
+    }
+
+
+       glutPostRedisplay();     
 }
+
 
 
 
@@ -59,35 +96,28 @@ void display(void)
      
   glLoadIdentity();
   
+    
+  
+  glRotatef(theta,1.0,0.0,0.0);
+  glRotatef(0,0.0,1.0,0.0);
+  glRotatef(20,0.0,0.0,1.0);
+  
+  glTranslatef(-0.5f+yt,-0.5f,-0.5f);
 
-  glRotatef(theta[0],1.0,0.0,0.0);
-  glRotatef(theta[1],0.0,1.0,0.0);
-  glRotatef(theta[2],0.0,0.0,1.0);
-   colorcube();
-   glFlush();
+     
+   polygon();
+   
    glutSwapBuffers();
 }
 
 
 
 
-void mouse(int btn,int state,int x,int y)
-{
-if(btn==GLUT_LEFT_BUTTON && state==GLUT_DOWN) 
-axis=0;//x axis
-if(btn==GLUT_MIDDLE_BUTTON && state==GLUT_DOWN)
-axis=1;//y axis
-if(btn==GLUT_RIGHT_BUTTON && state==GLUT_DOWN) 
-axis=2;//z axis
-}
 
-
-
-
-void myReshape(int a,int b)
+void myInit()
 {
    glMatrixMode(GL_PROJECTION);
-   glOrtho(-2.0,2.0,-2.0,2.0,-10.0,10.0);
+   glOrtho(-2.0,2.0,-2.0,2.0,-2.0,2.0);
    glMatrixMode(GL_MODELVIEW);
 }
 
@@ -98,12 +128,12 @@ int main(int argc,char** argv)
 {
             glutInit(&argc,argv);
             glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
-            glutInitWindowSize(500,500);
+            glutInitWindowSize(1000,1000);
             glutCreateWindow("CUBE");
-            glutReshapeFunc(myReshape);
+            myInit();
             glutDisplayFunc(display);
-	    glutIdleFunc(spincube);
-            glutMouseFunc(mouse);
+	          glutIdleFunc(translate);
+
             glEnable(GL_DEPTH_TEST);
             glutMainLoop();
             return 0;
